07bf98457ca40ba9ea8d18bec7ce886955d993bd
Jessica Zhang <jessz@umich.edu>
2020-04-14 20:09:42 -0500

    API Air Quality

--- Final-Project.py
+++ Final-Project.py
@@ -5,6 +5,8 @@ import os
 import requests
 
 #Names: Jessica Zhang 
+#Project Topic: How the Coronavirus is affecting CO2 Levels in the Atmosphere in Delhi, India (using AQI) and Int'l Flights 
+API_AQ = '851a8ae7-0eac-4f3a-8065-2e76906bc7f6'
 
 #sets up database 
 def setUpDatabase(db_name):
@@ -12,6 +14,12 @@ def setUpDatabase(db_name):
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
+
+def create_request_url_AQ(city):
+    #http://api.airvisual.com/v2/nearest_city?key=your_key
+    request_url = 'http://api.airvisual.com/v2/' + city + '?key=' + API_AQ 
+    return request_url
+
 
 def setUpTable1(data, cur, conn):
 
