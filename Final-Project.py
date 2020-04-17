37424b4723e7bde8fa6444ea29bd3596d3df3a59
Jessica Zhang <jessz@umich.edu>
2020-04-16 19:27:23 -0500

    setup tables

--- Final-Project.py
+++ Final-Project.py
@@ -6,7 +6,7 @@ import requests
 
 #Names: Jessica Zhang 
 #Project Topic: How the Coronavirus is affecting CO2 Levels in the Atmosphere in Delhi, India (using AQI) and Int'l Flights 
-API_AQ = '851a8ae7-0eac-4f3a-8065-2e76906bc7f6'
+API_AQ = 'berryram97'
 
 #sets up database 
 def setUpDatabase(db_name):
@@ -15,16 +15,46 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def create_request_url_AQ(city):
-    #http://api.airvisual.com/v2/nearest_city?key=your_key
-    request_url = 'http://api.airvisual.com/v2/' + city + '?key=' + API_AQ 
+#dates are in YYYYMMDD format
+#state = 2 digit state FIPS
+#county = 3 digit state FIPS code for the county
+#email is the requester's
+def create_request_url_AQ(email, param, bdate, edate, state_num, county_num):
+    #https://aqs.epa.gov/data/api/sampleData/byCounty?email=test@aqs.api&key=test&param=88101&bdate=20160101&edate=20160228&state=37&county=183    request_url = 'https://api.breezometer.com/air-quality/v2/historical/hourly?lat=' + latitude + '&lon=' + longitude + '&key=' API_AQ + '&start_datetime=' + start_date + '&end_datetime=' + end_date
+    request_url = 'https://aqs.epa.gov/data/api/sampleData/byCounty?email=' + email + '&key=' + API_AQ + '&param=' + str(param) + '&bdate=' + str(bdate) + '&edate=' + str(edate) + '&state=' + str(state_num) + '&county=' + str(county_num)
     return request_url
+    
+def get_AQ_data(email, param, bdate, edate, state_num, county_num):
+    request_url = create_request_url_AQ(email, param, bdate, edate, state_num, county_num)
+
+    results = requests.get(request_url)
+    r = json.loads(results.text)
+    print(json.dumps(r))
+    
+                  
 
 
-def setUpTable1(data, cur, conn):
+# def setUpTableAQ(data, cur, conn):
 
-    cur.execute("DROP TABLE IF EXISTS __________")
-    cur.execute("CREATE TABLE _________ (id INTEGER PRIMARY KEY, title TEXT)")
-    conn.commit()
+#     cur.execute("DROP TABLE IF EXISTS Air Quality")
+#     cur.execute('''
+#         CREATE TABLE IF NOT EXISTS Air Quality 
+#             ( TEXT PRIMARY KEY, 
+#             name TEXT, 
+#             address TEXT, 
+#             zip TEXT, 
+#             category_id INTEGER, 
+#     ''')
+#     conn.commit()
 
     pass 
+
+
+
+def main():
+    get_AQ_data('jessz@umich.edu', 88101, 20200101, 20200413, 36, '061')
+
+
+
+if __name__ == "__main__":
+    main()
