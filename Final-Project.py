cd6078ba5d53bbd9bac32df96fbbb5601270f742
Jessica Zhang <jessz@umich.edu>
2020-04-27 20:15:06 -0500

    visuals

--- Final-Project.py
+++ Final-Project.py
@@ -3,7 +3,9 @@ import sqlite3
 import json
 import os
 import requests
-import pdb
+import pandas 
+import matplotlib
+import matplotlib.pyplot as plt
 
 #Names: Jessica Zhang 
 #Project Topic: How the Coronavirus is affecting CO2 Levels in the Atmosphere in Delhi, India (using AQI) and Int'l Flights 
@@ -168,43 +170,174 @@ def get_COVID_country(cur, conn):
     print(results4)
 
 
+#CALCULATIONS
+def jointables(cur, conn):
+    cur.execute('''
+                SELECT Readings.date_local, Readings.reading, Readings.county_id
+                FROM Readings
+                JOIN County  
+                WHERE Restaurants.county_id = County.county_id
+                 '''
+    )
+    conn.commit()
+
+    results = cur.fetchall()
+    return results 
 
 
 
 
-# class TestAllMethods(unittest.TestCase):
+#VISUALIZATIONS
+def get_AQ_dates(cur,conn):
+    cur.execute( '''
+        SELECT date_local, reading
+        FROM Readings
+        WHERE date_local >= "2020-01-01"
+        AND date_local < "2020-04-20" 
+    ''')
+    conn.commit()
+    dates_data = cur.fetchall()
+    list_dates = []
+    for i in dates_data:
+        yearData = i[0]
+        list_dates.append(yearData)
+    return list_dates
 
-    # def setUp(self):
-    #     path = os.path.dirname(os.path.abspath(__file__))
-    #     self.conn = sqlite3.connect(path+'/'+__file__)
-    #     self.cur = self.conn.cursor()
+def get_AQ_readings(cur, conn):
+    cur.execute( '''
+        SELECT reading
+        FROM Readings
+    ''')
+    conn.commit()
+    reading_data = cur.fetchall()
+    list_readings = []
+    for i in reading_data:
+        if i[0] == None :
+            list_readings.append(0)
+        else :
+            list_readings.append(float(i[0]))
+    return list_readings
 
-  
+def get_covid_dates(cur, conn):
+    cur.execute( '''
+    SELECT Date
+    FROM Covid
+    ''')
+    conn.commit()
+    case_dates = cur.fetchall()
+    list_date_time = []
+    list_date = []
+    for i in case_dates:
+        list_date_time.append(i[0])
+    for i in list_date_time:
+        list_date.append(i[:10])
+    return list_date
+
+def get_covid_cases(cur, conn):
+    cur.execute( '''
+    SELECT Cases
+    FROM Covid
+    ''')
+    conn.commit()
+    cases = cur.fetchall()
+    list_cases = []
+    for i in cases:
+        list_cases.append(float(i[0]))
+    return list_cases
 
 
-    # def test_table_state(self):
-    #     cur.execute('SELECT * state_id, state_name FROM State')
-    #     list_state = cur.fetchall()
-    #     conn.commit()
+def createscatterplotsAQ(cur, conn):
+    x_values_unsorted = get_AQ_dates(cur,conn)
+    y_values_unsorted = get_AQ_readings(cur, conn)
 
-    #     self.assertEqual(list_state[0][0], "36")
+    list_sorted = []
+    for i in range(len(x_values_unsorted)):
+        list_sorted.append([x_values_unsorted[i], y_values_unsorted[i]])
+    list_sorted = sorted(list_sorted, key = lambda x:x[0])
+
+    x_val = []
+    y_val = []
+    for item in list_sorted:
+        x_val.append(item[0])
+        y_val.append(item[1])
+
+    second_x_unsorted = get_covid_dates(cur,conn)
+    second_y_unsorted = get_covid_cases(cur,conn)
+    list_sorted_two = []
+    for i in range(len(x_values_unsorted)):
+        list_sorted_two.append([second_x_unsorted[i], second_y_unsorted[i]])
+    list_sorted_two = sorted(list_sorted_two, key = lambda x:x[0])
+    x = []
+    y = []
+    for item in list_sorted_two:
+        x.append(item[0])
+        y.append(item[1])
+
+
+    ax = plt.gca()
+    plt.scatter(x_val, y_val)
+    plt.scatter(x, y)
+
+def create_area_plot(cur,conn):
+    x_values_unsorted = get_AQ_dates(cur,conn)
+    y_values_unsorted = get_AQ_readings(cur, conn)
+
+    list_sorted = []
+    for i in range(len(x_values_unsorted)):
+        list_sorted.append([x_values_unsorted[i], y_values_unsorted[i]])
+    list_sorted = sorted(list_sorted, key = lambda x:x[0])
+
+    x_val = []
+    y_val = []
+    for item in list_sorted:
+        x_val.append(item[0])
+        y_val.append(item[1])
+
+    second_x_unsorted = get_covid_dates(cur,conn)
+    second_y_unsorted = get_covid_cases(cur,conn)
+    list_sorted_two = []
+    for i in range(len(x_values_unsorted)):
+        list_sorted_two.append([second_x_unsorted[i], second_y_unsorted[i]])
+    list_sorted_two = sorted(list_sorted_two, key = lambda x:x[0])
+    x = []
+    y = []
+    for item in list_sorted_two:
+        x.append(item[0])
+        y.append(item[1])
+    
+    
+    plt.scatter(x_val, y_val, c='purple', alpha=0.2)
+    plt.plot(x,y)
+    plt.fill_between(x,y, color="skyblue", alpha = 0.3)
+    plt.title("CoronaVirus Cases")
+    
+
+    plt.show()
+    #plt.savefig("plt.png")
+     
+
+
+
 
 def main():
     cur, conn = setUpDatabase('Readings.db')
-    setUpTableCounty('jessz@umich.edu', '88101', '20200101', '20200415', '36', '061', cur, conn)
-    setUpReadingsTable('jessz@umich.edu', '88101', '20200101', '20200415', '36', '061', cur, conn)
-    setUpTableState('jessz@umich.edu', '88101', '20200101', '20200415', '36', '061', cur, conn)
+    setUpTableCounty('jessz@umich.edu', '88101', '20200101', '20200415', '17', '031', cur, conn)
+    setUpReadingsTable('jessz@umich.edu', '88101', '20200101', '20200415', '17', '031', cur, conn)
+    setUpTableState('jessz@umich.edu', '88101', '20200101', '20200415', '17', '031', cur, conn)
 
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '17', '031', cur, conn)
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '17', '031', cur, conn)
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '17', '031', cur, conn)
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '17', '031', cur, conn)
+    insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '17', '031', cur, conn)
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '36', '061', cur, conn)
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '36', '061', cur, conn)
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '36', '061', cur, conn)
     insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '36', '061', cur, conn)
-
-
+    insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '26', '163', cur, conn)
+    insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '26', '163', cur, conn)
+    insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '26', '163', cur, conn)
+    insertIntoReadingsTable('jessz@umich.edu', '88101', '20200101', '20200419', '26', '163', cur, conn)
 
 
 
@@ -222,28 +355,15 @@ def main():
     insertIntoC19Table('Italy', 'confirmed', cur, conn)
 
 
-
     get_readings(cur, conn)
     get_state(cur, conn)
     get_county(cur, conn) 
     get_COVID_country(cur, conn)
+    
+    createscatterplotsAQ(cur, conn)
+    create_area_plot(cur, conn)
 
-
-
-
-#CALCULATIONS
-def jointables(cur, conn):
-    cur.execute('''
-                SELECT Readings.date_local, Readings.reading, Readings.county_id
-                FROM Readings
-                JOIN County  
-                WHERE Restaurants.county_id = County.county_id
-                 '''
-    )
-
-    results = cur.fetchall()
-    return results 
-
+    
 
 
 
